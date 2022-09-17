ed6f8787bff17efb1f8f59e4ce2ec3f76583c7f4
kathgaro <kg.ruizojeda@gmail.com>
2022-09-17 19:30:31 -0300

    Estilos modificados - modal active

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList, TouchableOpacity, Modal, } from 'react-native';
 
+
 // Estilos creados como un objetos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-   justifyContent: 'center',
-   alignItems: 'center',
+  // justifyContent: 'center',
+   //alignItems: 'center',
   },
   inputContainer:{
     marginTop: 50,
@@ -15,18 +16,23 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
    flexDirection: 'row',
    justifyContent: 'space-between',
-   
-
   },
   input: {
+    width: '75%',
     //marginBottom: 25,
     borderBottomColor: '#2C514C',
     borderBottomWidth: 1,
     height: 40,
     color: '#2C514C',
+    //flex: 1,
+    //marginRight: 20,
+   // fontSize: 20,
+  },
+  itemList:{
+    //ocupa todo el espacio disponible
     flex: 1,
-    marginRight: 20,
-    fontSize: 20,
+    marginHorizontal: 20,
+    marginVertical: 20,
   },
   itemContainer:{
     flex: 1,
@@ -37,161 +43,100 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingHorizontal: 10,
     paddingVertical: 20,
-    borderRadius: 6,
-    width: '74%',
+    borderRadius: 5,
+   // width: '74%',
   },
   item:{
    color: 'white',
    fontSize: 16,
-   
   },
-  itemList:{
-    //ocupa todo el espacio disponible
-    flex: 1,
-    marginHorizontal: 20,
-    marginVertical: 20,
-  },
+ 
   delete: {
-    fontSize: 14,
-    fontWeight: '700',
+    fontSize: 18,
+    fontWeight: 'bold',
     color: 'white',
   },
   modalContainer: {
     justifyContent: 'center',
-    alignItems: 20,
+    alignItems: 'center',
     paddingVertical: 20,
-    marginTop: 50,
+    marginTop: 30,
   },
   modalTitle: {
     fontSize: 16,
   },
-  modalMessage:{
+  modalMessageContainer:{
     justifyContent: 'center',
     alignItems: 'center',
     paddingVertical: 10,
-  }, 
-  modalMessageContainer:{
-    fontSize: 14,
   },
+  modalMessage:{
+    fontSize: 14,
+  }, 
+ 
   selectedTask:{
     fontSize: 16,
     color: 'black',
-    fontWeight: 700,
+    fontWeight: 'bold',
+  },
+  buttonContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginHorizontal: 20,
   }
  
 });
 
 //sintaxis de creacion de componentes - jsx
 export default function App() {
-// P A S O 1
-  //Almacenamos lo que se ingresa en el input - En este caso en una memoria (ya que aun no tengo una api ni una base de datos), es decir, "en un estado de tiempo de ejecucion"
-  //Debo setear 2 estados - 1. Obtengo/sete/asigno lo que he escrito en el "input" y 2. Arreglo con valores que he "guardado" dentro del "input" que almacenamos aparte para luego poder visualizarlo.
-  //En react native -mobile- no existe el concepto "formulario" sino "campo editable" se hacen "edit Text"
 
-  //  Iniciando nuestro estado, inicia vacio.
-  //             ↱ Funcion que me permite asignar un valor del estado
-  //                        ↱ Inicio mi estado como cadena/string vacio (porque mi input recibe texto)
   const [task, setTask] = useState(''); 
-//        ↳ Lo que ingreso dentro de "input", estado inicial
-
-//  Inicio los datos que recibire, inicia vacio.
   const [tasks, setTasks] = useState([]);
   
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
 
+  
   const onHandleChangeText = (text) => {
-    setTask(text)
+    setTask(text);
   }
-  // Corroboro que funciona
-  console.warn('task', task)
-  console.warn('tasks', tasks)
 
-
-
-  // Limpiando el input al agregar
-      // Opcion manejando un arreglo
-  const addItem = () => {
-    //  ↱ Asigno grupo de tareas
-    //            ↱ Copia de arreglo vacio       
+  const addItem = () => {   
     setTasks((prevTasks) =>[
       ...prevTasks, 
-      // ↱ Nuevo con id para identificar un valor generico 
-      {
-        // Math.random().toString() -> Numero random
-        // tasks.length + 1 -> Registro consevutivo
-        // tasks[tasks.length -1].id + 1 -> Acceso al ultimo id del arreglo y empiezo a agregar uno mas. Me devuelve erro al iniciar en 0
-        // Date.now
-
-        id:tasks.length + 1 ,
-        //Asigno valor de campo, del estado
-        value: task,
-      }
+      {id:tasks.length + 1 , value: task},
     ]);
     //limpio
-    setTask('')
+    setTask('');
   }
-
-  //Usando un arreglo de objetos
-  /**
-   *  OPCION MANEJANDO STRING
-   *      ↱ Nombre de mi funcion
-  const addItem = () => {
-      ↱ Asigno grupo de tareas
-                ↱ Copia de tarea
-                        ↱ Nuevo valor de tarea        
-    setTask([...tasks, task])
-  }
-   */
-
-/**
- * Boton que use antes:
- *  <TextInput 
-        placeholder='Ingresa tu nombre' 
-        style={styles.input} 
-        selectionColor='#E3C0D3'
-        /> 
-        
- */
-
-//    RENDER - MOSTRANDO EN PANTALLA LOS DATOS OBTENIDOS
-/**
- * hacemos un map para recorer la lista -linea 66
- */
 
 const onHandleModal = ( id ) => {
   setModalVisible(!modalVisible);
   setSelectedTask(tasks.find((item) => item.id == id))
-  console.log(id);
+  console.warn(id);
 }
 const renderItem = ({item}) => (
- 
-          <View
-          key={`item-${item.id}`} 
-          style={styles.itemContainer}
-          >
-            <Text style={styles.item}> {item.value} </Text>
-
-          <TouchableOpacity 
-          //Elimino item de lista
-          onPress={() => onHandleModal(item.id)}>
-            <Text style={styles.delete}> x </Text>
-          </TouchableOpacity>
-
-          </View>
-        
+  <View style={styles.itemContainer}>
+    <Text style={styles.item}> {item.value} </Text>
+    <TouchableOpacity onPress={() => onHandleModal(item.id)}>
+      <Text style={styles.delete}> x </Text>
+    </TouchableOpacity>
+  </View>      
 )
+
+const onHandleDeleteItem = (id) =>{
+  setTasks(tasks.filter((item) => item.id !== id));
+  setSelectedTask(null);
+  setModalVisible(!modalVisible);
+}
   return (
     //View - contenedor principal
     <View style={styles.container}> 
-     
       <View style={styles.inputContainer}>
         <TextInput 
-        //TextInput - Tiene muchos eventos 
         placeholder='Ingresa texto ' 
         style={styles.input} 
         selectionColor='#E3C0D3'
-        //Me retorna el valor de lo que escribo en el campo
         onChangeText={onHandleChangeText}
         //Asigno el valor del estado
         value={task}
@@ -206,23 +151,7 @@ const renderItem = ({item}) => (
         onPress={addItem} 
         color='#2C514C'/>
       </View>
-      <View>
       
-        {        /**tasks.map((item) => (
-          //                ↳ Me devuelve un componente "view"
-          //          ↳ Me devuelve cada item
-         //   ↳ Recorro la lista de mi arreglo
-
-          <View 
-          //React necesita un identificados unico para cada uno de los arreglos que estamos recorriendo por eso usamos una "key", le paso el valor "item.id" para cada elemento de mi arreglo.
-          key={`item-${item.id}`} 
-          style={styles.itemContainer}
-          >
-            <Text style={styles.item}> {item.value} </Text>
-          </View>
-        )) COMENTA PORQUE USAR LO SIGUIENTE ("FLATLIST")*/}
-        </View>
-
         <FlatList
           style={styles.itemList}
           data={tasks}
@@ -245,8 +174,13 @@ const renderItem = ({item}) => (
             <Button 
               title='eliminar'
               onPress={() => onHandleDeleteItem(selectedTask?.id)}
-              
-            ></Button>
+              color='gray'
+             />
+             <Button 
+              title='Cancelar'
+              onPress={() => setModalVisible(!modalVisible)}
+              color='black'
+             />
           </View>
         </Modal>
     </View>
